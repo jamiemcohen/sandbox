91c262da57b9c9d32e97e69d573b86c4b0f90a84
Jamie Cohen <JAMIE.M.COHEN@VANDERBILT.EDU>
2017-07-06 16:29:20 -0500

    started create controller

--- public/app.js
+++ public/app.js
@@ -28,3 +28,21 @@ function($scope) {
   ];
 }]);
 
+app.controller('markerMakerController', [
+'$scope', function($scope){
+    $scope.title ="";
+    $scope.description = "";
+    $scope.latitude = "";
+    $scope.longitude ="";
+    $scope.saveMarker = function(){
+        $scope.latitude = $("#latitude").val(); 
+        $scope.longitude = $("#longitude").val();
+        console.log("New Marker: \n" + 
+              "Title: " + $scope.title + 
+              "\nDescription: " + $scope.description +
+              "\nLatitude: "+ $scope.latitude +
+              "\nLongitude: "+ $scope.longitude);    
+        //now save and plot this data
+    }
+    
+}]);
